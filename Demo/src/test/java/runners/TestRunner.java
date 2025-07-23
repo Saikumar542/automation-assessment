4dd52212835be19128f8709d94e60bed921f259a
Saikumar <saikumarsankara@gmail.com>
2025-07-23 09:01:21 +0530

    Modified testrunnerfile

--- Demo/src/test/java/runners/TestRunner.java
+++ Demo/src/test/java/runners/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		features = "C:\\Users\\saiku\\Music\\automation-assessment\\Demo\\src\\test\\resources\\features\\ApiLogin.feature",
+		features = "C:\\Users\\saiku\\Music\\Assinment\\automation-assessment\\Demo\\src\\test\\resources\\features",
 		glue = "stepdefinitions",
 		plugin = {"pretty", "html:target/cucumber-report.html"},
 		monochrome = true
